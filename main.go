6bab1ea03e7586b843eabd58f00f7fabeb9205ff
erulan14 <erulan14@yandex.ru>
2024-12-04 11:08:49 +0500

    add factorial function and goroutine

--- main.go
+++ main.go
@@ -2,6 +2,25 @@ package main
 
 import "fmt"
 
+func factorial(n int) {
+	if(n < 1){
+		fmt.Println("Invalid input number")
+		return
+	}
+	result := 1
+	for i := 1; i <= n; i++{
+		result *= i
+	}
+	fmt.Println(n, "-", result)
+}
+
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Run app")
+
+	for i := 1; i <= 10; i++ {
+		go factorial(i)
+	}
+
+	fmt.Scanln()
+	fmt.Println("The End")
 }
